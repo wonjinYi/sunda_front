4cc494e2173967b2c925cff004b45c381501fb1b
wonjinYi <studioplug17@gmail.com>
2020-10-13 22:08:13 +0900

    change css to scss , and i am writing code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,23 @@
 import React from "react";
-//import "./Navigation.css";
+import "./Navigation.scss";
 
 import Logo from "../img/sunda_logo.png";
+import Icon_search from "../img/icon/search.png";
 
 function Navigation() {
     return (
         <div className="nav">
             <div className="logo"><img src={Logo} alt="sunda logo"></img></div>
             <div className="search">
-                <input type="text" title="검색" className="search_input"></input>
-                <button type="button" className="search_button">검색</button>
+                <select className="category">
+                    <option value="title">Title</option>
+                    <option value="artist">Artist</option>
+                    <option value="curator">Curator</option>
+                </select>
+                <input type="text" title="Search Awesome Curation" className="search_input"></input>
+                <button type="button" className="search_button">
+                    <img src={Icon_search} alt="search button"></img>
+                </button>
             </div>
             <div className="menu">
                 <a>Sign up</a>
