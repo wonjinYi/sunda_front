cc81243076eaa2bf6348ca7e2a7602c6ee0251b4
wonjinYi <studioplug17@gmail.com>
2020-10-15 19:43:13 +0900

    function component -> class component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,27 +6,30 @@ import "./Navigation.scss";
 import Logo from "../img/sunda_logo.png";
 import Icon_search from "../img/icon/search_white.png";
 
-function Navigation() {
-    return (
-        <div className="nav">
-            <div className="logo"><img src={Logo} alt="sunda logo"></img></div>
-            <div className="search">
-                <select className="category">
-                    <option value="title">Title</option>
-                    <option value="artist">Artist</option>
-                    <option value="curator">Curator</option>
-                </select>
-                <input type="text" placeholder="Search Awesome Curation" className="input"></input>
-                <button type="button" className="button">
-                    <img src={Icon_search} alt="search button"></img>
-                </button>
+class Navigation extends React.Component {
+    render () {
+        return (
+            <div className="nav">
+                <div className="logo"><img src={Logo} alt="sunda logo"></img></div>
+                <div className="search">
+                    <select className="category">
+                        <option value="title">Title</option>
+                        <option value="artist">Artist</option>
+                        <option value="curator">Curator</option>
+                    </select>
+                    <input type="text" placeholder="Search Awesome Curation" className="input"></input>
+                    <button type="button" className="button">
+                        <img src={Icon_search} alt="search button"></img>
+                    </button>
+                </div>
+                <div className="menu">
+                    <Link><div className="signup common-align">Sign up</div></Link>
+                    <Link><div className="signin common-align">Sign in</div></Link>
+                </div>
             </div>
-            <div className="menu">
-                <Link><div className="signup common-align">Sign up</div></Link>
-                <Link><div className="signin common-align">Sign in</div></Link>
-            </div>
-        </div>
-    );
+        );
+    }
+    
 }
 
 export default Navigation;
