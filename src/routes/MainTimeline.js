29d6aeca0ec53894e100913a3c797483cc9347f4
wonjinYi <studioplug17@gmail.com>
2020-10-17 13:12:13 +0900

    set state loggedin:true temporaryiyt

--- src/routes/MainTimeline.js
+++ src/routes/MainTimeline.js
@@ -3,14 +3,17 @@ import Navigation from '../components/Navigation';
 
 class MainTimeline extends React.Component{
     state = {
-
+        loggedin : true,
+        test : 123,
     };
 
     render () {
+        console.log(this.state.loggedin);
         return (
             <div>
-                <Navigation />
-                <h1>Here is MainTImeline</h1>
+                <Navigation loggedin={this.state.loggedin} />
+                <h1>Here is MainTImeline dd {this.state.loggedin} {this.state.test}</h1>
+                <h2>{this.state.loggedin}d</h2>
             </div>
            
         ); 
