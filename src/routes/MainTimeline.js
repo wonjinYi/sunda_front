1d5a2a7aedbab7d9330d346ac183c1959735c116
wonjinYi <studioplug17@gmail.com>
2020-10-18 22:56:08 +0900

    temp changed

--- src/routes/MainTimeline.js
+++ src/routes/MainTimeline.js
@@ -4,15 +4,18 @@ import Navigation from '../components/Navigation';
 class MainTimeline extends React.Component{
     state = {
         loggedin : true,
+        username : "username",
         test : 123,
     };
 
     render () {
-        console.log(this.state.loggedin);
+        console.log(this.state);
+        const { loggedin, username } = this.state;
+
         return (
             <div>
-                <Navigation loggedin={this.state.loggedin} />
-                <h1>Here is MainTImeline dd {this.state.loggedin} {this.state.test}</h1>
+                <Navigation loggedin={loggedin} username={username}/>
+                <h1>Here is MainTImeline dd {this.state.loggedin}</h1>
                 <h2>{this.state.loggedin}d</h2>
             </div>
            
