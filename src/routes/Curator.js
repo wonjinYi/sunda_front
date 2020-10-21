6ef6a12466192a180e57b0100a862993d5a23844
wonjinYi <studioplug17@gmail.com>
2020-10-22 00:06:20 +0900

    initial set Curator routes

--- src/routes/Curator.js
+++ src/routes/Curator.js
@@ -2,6 +2,12 @@ import React from "react";
 
 class Curator extends React.Component {
     render () {
-    
+        return (
+            <div className="Curator">
+
+            </div>
+        );
     }
 }
+
+export default Curator;
