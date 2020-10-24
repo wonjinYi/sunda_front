c253017e9ccafb490f96c4367be610256560e660
wonjinYi <studioplug17@gmail.com>
2020-10-24 17:20:20 +0900

    add Signup router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// modules
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 
-import Navigation from "./components/Navigation";
+// routers
 import Landing from "./routes/Landing";
 import MainTimeline from "./routes/MainTimeline";
+import Signup from "./routes/Signup";
 
+// components
+import Navigation from "./components/Navigation";
+
+// style sheets
 import './App.css';
 
+// -------------- Import is end
 
 function App() {
   return ( 
@@ -14,6 +21,7 @@ function App() {
       <HashRouter>
         <Route path="/" exact={true} component={MainTimeline}/>
         <Route path="/landing" exact={true} component={Landing} />
+        <Route path="/signup" exact={true} component={Signup} />
       </HashRouter>
     </div>
   );
