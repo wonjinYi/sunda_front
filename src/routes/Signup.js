4059a88d593b528ab917f0bc5cef7a7c1725b324
wonjinYi <studioplug17@gmail.com>
2020-11-04 21:24:29 +0900

    signup step 0,2

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -11,27 +11,62 @@ import icon_naver from "../img/icon/sns_naver.png";
 import icon_kakao from "../img/icon/sns_kakao.png";
 
 class Signup extends React.Component {
-    render () {
-        return (
-            <div className="Signup">
+    state = {
+        step : 0,
+    };
 
-                <p className="description">Please Select Account type you will use in SUNDA</p>
-                <div className="with-other-services">
-                    <p>Sign up with other services</p>
-                    <div>
-                        <Link><img src={icon_daldalso} className="icon" alt="daldalso"></img></Link>
-                        <Link><img src={icon_google} className="icon" alt="google"></img></Link>
-                        <Link><img src={icon_facebook} className="icon" alt="facebook"></img></Link>
-                        <Link><img src={icon_naver} className="icon" alt="naver"></img></Link>
-                        <Link><img src={icon_kakao} className="icon" alt="kakao"></img></Link>
+    componentDidMount() {
+        const { step } = this.state;
+        const test=document.getElementsByClassName('new-account')[0];
+        test.addEventListener('click', () => {
+            this.setState({step:2});
+        })
+    }
+
+    render() {
+        const { step } = this.state;
+
+        if ( step == 0 ){
+            return (
+                <div className="Signup">
+                    <div className="with-other-services">
+                        <p className="caption">Sign up with other services</p>
+                        <div>
+                            <Link to="/"><img src={icon_daldalso} className="icon" alt="daldalso"></img></Link>
+                            <Link to="/"><img src={icon_google} className="icon" alt="google"></img></Link>
+                            <Link to="/"><img src={icon_facebook} className="icon" alt="facebook"></img></Link>
+                            <Link to="/"><img src={icon_naver} className="icon" alt="naver"></img></Link>
+                            <Link to="/"><img src={icon_kakao} className="icon" alt="kakao"></img></Link>
+                        </div>
+                    </div>
+                    <div className="with-sunda-account">
+                        <p className="caption">Sign up with SUNDA account</p>
+                        <p className="button-fill new-account">Create a New SUNDA account</p>
                     </div>
                 </div>
-                <div className="with-sunda-account">
-                    <p>Sign up with SUNDA account</p>
-                    <Link><p className="button-fill new-account">Create a New SUNDA account</p></Link>
+            );
+        }
+        else if ( step == 1 ){
+            return (
+                <div className="Signup">
+
                 </div>
-            </div>
-        );
+            );
+        }
+        else if ( step == 2 ){
+            return (
+                <div className="Signup">
+                    <p className="title highlight">Acceptance Letter</p>
+                    <div className="letter">
+                        <p>We received your application and have completed the review.</p>
+                        <p>Welcome to member of SUNDA! Show your curation to the World</p>
+                    </div>
+                    
+                    <Link to="/"><p className="button-fill end-signup">Go to SUNDA Timeline</p></Link>
+                </div>
+            );
+        }
+        
     }
 }
 
